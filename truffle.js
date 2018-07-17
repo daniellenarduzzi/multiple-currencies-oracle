60c6a493dd689d14619d335a656901baf4f680b3
castor <daniellenarduzzi@gmail.com>
2018-07-17 14:14:04 -0300

    added boilerplate, created contract

---
+++ truffle.js
@@ -0,0 +1,36 @@
+require('babel-register')({
+  ignore: /node_modules\/(?!zeppelin-solidity)/
+});
+require('babel-polyfill');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8545,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
+    ropsten: {
+      host: "localhost",
+      network_id: 3,
+      port: 8545,
+      gas: 2900000
+    },
+      kovan: {
+      host: kovan.cryptologic.io:8787,
+      port: 8747,
+      network_id: "*",
+      from: "0x44751576b07eee07de3d8d5bfb9c8dd77add1744"
+    },
+  },
+  mocha: {
+   useColors: true
+  }
+};
